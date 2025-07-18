abc433ea80a4e10f3681c0daf22933f7e594e521
bsahd <193844131+bsahd@users.noreply.github.com>
2025-07-18 13:10:25 +0900

    フォーマットを変更

--- index.mjs
+++ index.mjs
@@ -1,24 +1,24 @@
 import * as fs from "fs/promises";
 import * as readline from "readline/promises";
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const data = JSON.parse(await fs.readFile("data.json"));
 let question = data;
 while (true) {
-	if (question.answer) {
-		console.log(question.answer);
-		process.exit();
-		break;
-	}
-	const res = await rl.question(question.question);
-	if (res == "y") {
-		question = question.yes;
-	} else if (res === "n") {
-		question = question.no;
-	} else {
-		console.log("Invalid input. Please enter 'y' or 'n'.");
-	}
+  if (question.answer) {
+    console.log(question.answer);
+    process.exit();
+    break;
+  }
+  const res = await rl.question(question.question);
+  if (res == "y") {
+    question = question.yes;
+  } else if (res === "n") {
+    question = question.no;
+  } else {
+    console.log("Invalid input. Please enter 'y' or 'n'.");
+  }
 }
